47e4b77062a368cfc8db9de6d81fb9ebc8ace7bf
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-09-20 17:01:50 +0300

    Initial commit

---
+++ LogAnalyzerCLI.js
@@ -0,0 +1,8 @@
+/*
+** @Author: HamidReza Ghavami
+** @Date: 2025-09-20
+** @Description: A Node.js CLI tool to parse, analyze, and summarize log files, 
+**    helping you quickly identify errors, warnings, and key metrics.
+*/
+
+// comming soon...
