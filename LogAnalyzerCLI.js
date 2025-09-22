8e6880ebbe43b87038d202a1091bdd4ff7ef827f
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-09-22 21:45:12 +0300

    First changes

--- LogAnalyzerCLI.js
+++ LogAnalyzerCLI.js
@@ -1,8 +1,52 @@
-/*
-** @Author: HamidReza Ghavami
-** @Date: 2025-09-20
-** @Description: A Node.js CLI tool to parse, analyze, and summarize log files, 
-**    helping you quickly identify errors, warnings, and key metrics.
-*/
+import fs, { readFileSync } from 'fs';
+import path from 'path';
 
-// comming soon...
+// function read log files
+function readLogFiles(folderPath) { 
+    if (!fs.readFileSync(folderPath)) { 
+        console.log(`Folder does not exist ${folderPath}`);
+        return [];
+    }
+
+    // get all files in the folder
+    const files = fs.readdirSync(folderPath);
+
+    // filter .log files
+    const logFilters = files.filter(file => path.extname(file) === '.log');
+
+    // read each log file
+    const logs = logFiles.map(file => {
+        const fullPath = path.join(folderPath, file);
+        const content = fs.readFileSync(fullPath, 'utf-8');
+        return content;
+    });
+    return logs;
+}
+
+// function parse log 
+function parseLog(rawData) { 
+
+}
+
+// function Analyze Logs
+function analyzeLog(parsedLogs, filters) { 
+
+}
+
+// function format output
+function formatOutPut(analysisResult) {
+
+}
+
+// function Display
+function displayReport(formattedReport) { 
+
+}
+
+// function filter by date
+function filterByDate(logs, start, end) { 
+    sortBySeverity(logs);
+}
+
+const logs = readLogFiles('./logs');
+console.log(logs);
